32eafdd75efe3c30d6e18504955c45de73e24cff
luyaochi <ab123ab456g@gmail.com>
2014-10-08 04:28:58 +0800

    finish type1 and add write testunit

--- test_type1.py
+++ test_type1.py
@@ -2,3 +2,93 @@
 # -*-coding:utf-8-*-
 
 import unittest
+import urllib
+
+
+from type1 import crawler
+
+path = 'http://www.nchu.edu.tw/index1.php'
+
+class TestCrawler(unittest.TestCase):
+	def setUp(self):
+		self.crawler = crawler(path)
+
+	#test initialize variable
+	def test_path(self):
+		Unit1 = path
+		Unit2 = self.crawler.path
+		self.assertEqual(Unit1,Unit2)
+
+	def test_htmlCode_Undecode(self):
+		Unit1 = urllib.urlopen(path).read()
+		Unit2 = self.crawler.htmlCode_Undecode
+		self.assertEqual(Unit1,Unit2)
+
+	def test_htmlCode_decode(self):
+		shortName = self.crawler
+		Unit1 = shortName.htmlCode_Undecode.decode('utf8', 'ignore')
+		Unit2 = shortName.htmlCode_Decode
+		self.assertEqual(Unit1,Unit2)
+	
+	#not finish
+	#test get URL
+	#def test_getURL(self):
+	#	Unit1 = self.crawler.getURL()
+	#	Unit2 = self.crawler
+	#	self.assertEqual(Unit1,Unit2)
+	
+	#test get findUrl
+	def test_findUrl(self):
+		Unit1 = self.crawler.findUrl(0,0,'')
+		Unit2 = (5,4)
+		self.assertEqual(Unit1,Unit2)
+
+	#test filter url
+	def test_filter_Url(self):
+		Unit1 = self.crawler.filter_Url('123456')
+		Unit2 = self.crawler.path + '/123456'
+		self.assertEqual(Unit1,Unit2)
+
+	def test_filter_Url_with_slash(self):
+		Unit1 = self.crawler.filter_Url('/123456')
+		Unit2 = self.crawler.path + '/123456'
+		self.assertEqual(Unit1,Unit2)
+
+	def test_filter_Url_HTTPS(self):
+		Unit1 = self.crawler.filter_Url('HTTPS://123456')
+		Unit2 = 'HTTPS://123456'
+		self.assertEqual(Unit1,Unit2)
+
+	def test_filter_Url_https(self):
+		Unit1 = self.crawler.filter_Url('https//123456')
+		Unit2 = 'https//123456'
+		self.assertEqual(Unit1,Unit2)
+
+	def test_filter_Url_HTTP(self):
+		Unit1 = self.crawler.filter_Url('HTTP://123456')
+		Unit2 = 'HTTP://123456'
+		self.assertEqual(Unit1,Unit2)
+
+	def test_filter_Url_http(self):
+		Unit1 = self.crawler.filter_Url('http://123456')
+		Unit2 = 'http://123456'
+		self.assertEqual(Unit1,Unit2)
+
+	#def test_findUrl(self):
+	#	self.assertEqual(self.crawler.getHtmlCode(),self.crawler.htmlCode)
+
+	#def test_printpath(self):
+	#	saved_stdout = sys.stdout
+	#	try:
+	#		out = StringIO()
+	#		sys.stdout = out
+	#		self.crawler.printpath()
+	#		output = out.getvalue().strip()
+	#		assert output == 'http://www.nchu.edu.tw/\n["news.php?type=1&id=1","&page=0"\n{"pattern":"<font color="#999999">(.+?)</font>"}http://www.nchu.edu.tw/news.php?type=1&id=1&page=0"'
+	#	finally:
+	#		sys.stdout = saved_stdout
+
+if __name__ == '__main__':
+	#unittest.main()
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestCrawler)
+	unittest.TextTestRunner(verbosity=2).run(suite)
--- type1.py
+++ type1.py
@@ -3,41 +3,62 @@
 
 import urllib
 
-
-
 class crawler:
+'''
+	only crawl one page, and not use regex to filter word 
 
-	__init__(self,Path):
-		self.Path = Path
-		self.htmlCode_Undecode = urllib.urlopen(Path)
-		self.htmlCode_Decode = htmlCode_Undecode.read().decode('utf8', 'ignore')
-	
-	#find Url in the htmlCode
-	def findUrl(link_start,link_end,htmlCode):
-		link_start = htmlCode_Decode[link_start:].find('href=\"') + len('href=\"') + link_start
-		link_end = htmlCode_Decode[link_start:].find('\"') + link_start
-		if ".css" not in htmlCode_Decode[link_start:link_end]:
-			filter_Url(htmlCode_Decode[link_start:link_end])
-		return link_start,link_end
-	
-	#filter the URL do not have http or https
-	def filter_Url(Path_get):
-		if  "http:/" in  Path_get or "HTTP:/" in  Path_get :
-			print(Path_get)
-		elif "https:/" in Path_get or  "HTTPS:/" in Path_get:
-			print(Path_get)
-		else:
-			print(Path+ '/' +Path_get)
+'''
 
+	# init variable 
+	#self.path mean the path you want to crawller
+	#self.htmlCode_Undecode mean the html code you got before you decode
+	#self.htmlCode_Decode mean the html code you got after you decode
+
+	def __init__(self,path):
+
+		self.path = path
+		self.htmlCode_Undecode = urllib.urlopen(path).read()
+		self.htmlCode_Decode = self.htmlCode_Undecode.decode('utf8', 'ignore')
+	
 	#get all URL in onepage
-	def getURL():
+	def getURL(self):
+
+		(link_start,link_end) = (0,0)
+		htmlCode = self.htmlCode_Decode
+
 		while 1:
-			if 'href=\"' not in htmlCode_Decode[link_start:] and 'HREF=\"' not in htmlCode_Decode[link_start:]:
-			break
+			shortName = self.htmlCode_Decode[link_start:]
+			if 'href=\"' not in shortName and 'HREF=\"' not in shortName:
+				break
+			link_start,link_end = self.findUrl(link_start, link_end, htmlCode)
 
-		link_start,link_end = findUrl(link_start, link_end, htmlCode_Decode)
+	#find Url in the htmlCode
+	def findUrl(self, link_start, link_end, htmlCode):
+		#let one line shorter than 80 column
+		link_start = len('href=\"') + link_start
+		link_start = htmlCode[link_start:].find('href=\"') + link_start
+
+		link_end = htmlCode[link_start:].find('\"') + link_start
+		if ".css" not in htmlCode[link_start:link_end]:
+			print(self.filter_Url( htmlCode[link_start:link_end] ))
+		return link_start,link_end
+
+	#filter the URL do not have http or https
+	def filter_Url(self,Path_get):
+		if  "http" in  Path_get or "HTTP" in  Path_get :
+			return Path_get
+		elif "https" in Path_get or  "HTTPS" in Path_get:
+			return Path_get
+		else:
+			Pos = Path_get.find('/')
+			if Pos == 0:
+				Path_get = Path_get[1:]
+			return self.path + '/' + Path_get
 
 
 
-
-
+#a = crawler('http://www.nchu.edu.tw/index1.php')
+#print(a.path)
+#print(a.htmlCode_Decode)
+#print(a.htmlCode_Undecode)
+#a.getURL()
